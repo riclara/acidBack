661a91d2475db8eed2b09f97b2b88b553a5dc7a3
Ricardo Lara <riclara@gmail.com>
2019-02-23 21:07:18 -0500

    update external network name

--- server/app.js
+++ server/app.js
@@ -1,37 +1,39 @@
 import DarkSky from 'dark-sky'
 import express from 'express'
 import Redis from './redis'
+import cors from 'cors'
 
 const app = express()
 const port = 8000
 const darksky = new DarkSky(process.env.DARK_SKY)
 const redis = new Redis(13835, 'redis-13835.c16.us-east-1-3.ec2.cloud.redislabs.com')
 
-async function query ({latitude, longitude}) {
+app.use(cors())
+
+async function query ({country, latitude, longitude}) {
   try {
     const randomnum = Math.floor((Math.random() * 9
     ))
     console.log('randomnum: ', randomnum)
     if (!randomnum) throw new Error('Random error')
-    const cache = await redis.get(latitude + longitude)
-    console.log('cache: ', cache)
+    const cache = await redis.get(country)
     if (cache) return cache
     const data = await darksky
       .latitude(latitude)
       .longitude(longitude)
       .get()
-    if (data && data.currently) redis.set(latitude + longitude, data)
+    if (data && data.currently) redis.set(country, data)
     return data
   } catch (error) {
     console.log('error: ', error)
     if (error instanceof Error && error.message === 'Random error') {
-      return query({latitude, longitude})
+      return query({country, latitude, longitude})
     }
     return error
   }
 }
 
-app.get('/latitude/:latitude/longitude/:longitude', async (request, response) => {
+app.get('/v1/:country/:latitude/:longitude', async (request, response) => {
   query(request.params)
     .then(data => {
       response.json(data)
